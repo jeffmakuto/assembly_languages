ded076e19792b5c2c3a628963223433d42ea0e13
jeffmakuto <hjeffmakuto@gmail.com>
2024-02-29 18:05:22 +0300

    Simple exit program

---
+++ 32-bit/x86/exit.asm
@@ -0,0 +1,26 @@
+; Purpose: Simple program that exits and returns a status
+;           code back to the Linux kernel
+;
+; Input: none
+;
+; Output: returns a status code. Can be viewed by typing
+;           echo $?
+;
+; Variables:
+;   %eax holds the system call number
+;   %ebx holds the return status
+;
+
+section .data
+section .text
+global _start
+
+_start:
+    ; Set up system call parameters
+    mov eax, 1      ; system call number for exit
+    mov ebx, 0      ; exit status code (0 for sucess)
+
+    ; Invoke kernel to exit the program
+    int 0x80        ; software interrupt to trigger the kernel
+
+    ; The program will not reach here as it has exited
